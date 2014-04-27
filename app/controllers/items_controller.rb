58e677b104afae61befa8d0edd0d4a2bad332df9
Nick Klesaris <nickklesaris@gmail.com>
2014-04-27 15:16:19 -0400

    fixed some glitches

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,17 @@ class ItemsController < ApplicationController
     @items = @session.items
   end
 
+  def create
+    @session = Session.find(params[:session_id])
+    @item = @session.items.build(item_params)
+      if @item.save
+        redirect_to new_session_item_path
+        flash[:notice] = "Item created."
+      else
+        render 'new'
+      end
+  end
+
   private
     def item_params
       params.require(:item).permit(:name, :type, :grouping_1, :grouping_2, :grouping_3, :grouping_4, :grouping_5, :grouping_6, :grouping_7, :grouping_8, :grouping_9, :grouping_10, :grouping_11, :grouping_12).merge(session_id: params[:session_id])
