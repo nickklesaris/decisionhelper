0964d1f54730e8319feb33a4d71a7ca5ecac6566
Nick Klesaris <nickklesaris@gmail.com>
2014-04-28 05:28:30 -0400

    Importance now used to calculate score

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,16 +22,11 @@ class SessionsController < ApplicationController
     @session = Session.find(params[:id])
     @item = Item.new
     @items = @session.items
-    @convert = {"Awful" => 8, "Poor" => 28, "Okay" => 58, "Great" => 84, "Outstanding" => 96, "unused" => 0 }
-    #count = 1
-    #while count <= 12
-
-    #end
-    #@score = 96.0
+    @convert = {"Awful" => 8, "Poor" => 28, "Okay" => 58, "Great" => 84, "Outstanding" => 96, "unused" => 1 }
   end
 
   private
     def session_params
-      params.require(:session).permit(:name, :description, :item_type, :criterion_1, :criterion_2, :criterion_3, :criterion_4, :criterion_5, :criterion_6, :criterion_7, :criterion_8, :criterion_9, :criterion_10, :criterion_11, :criterion_12)
+      params.require(:session).permit(:name, :description, :item_type, :criterion_1, :criterion_2, :criterion_3, :criterion_4, :criterion_5, :criterion_6, :criterion_7, :criterion_8, :criterion_9, :criterion_10, :criterion_11, :criterion_12, :criterion_12, :importance_1, :importance_2, :importance_3, :importance_4, :importance_5, :importance_6, :importance_7, :importance_8, :importance_9, :importance_10, :importance_11, :importance_12)
     end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,5 @@
 class Session < ActiveRecord::Base
   has_many :items, dependent: :destroy
-  validates :name, presence: true, uniqueness: {cas_sensitive: false}
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :item_type, presence: true
 end
